2e174f968d47a130c226f5caeb0b0020896e0893
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-05-06 22:20:20 +0200

    MachineUpdate

--- VendingMachineSpectre/Machine.cs
+++ VendingMachineSpectre/Machine.cs
@@ -2,6 +2,18 @@
 
 public class Machine
 {
+    #region Fullscreen Variables
+    [DllImport("kernel32.dll", ExactSpelling = true)]
+    private static extern IntPtr GetConsoleWindow();
+    private static IntPtr ThisConsole = GetConsoleWindow();
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    private const int HIDE = 0;
+    private const int MAXIMIZE = 3;
+    private const int MINIMIZE = 6;
+    private const int RESTORE = 9;
+    #endregion
+
     public static void TableBuild(Table table)
     {
         table.AddColumn("Products");
@@ -13,8 +25,15 @@ public class Machine
 
         TableBuild(MachineTable);
         AnsiConsole.Write(MachineTable);
-
+        
+        
 
         Console.ReadKey();
+    }
+
+    public static void TableBuild(Table table)
+    {
+        table.AddColumn("Products");
+        table.AddRow("Test");
     }
 }
