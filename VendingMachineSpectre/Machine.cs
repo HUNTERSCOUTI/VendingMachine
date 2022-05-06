148c6cb425120a2c822c6ee17e088e9991d00be4
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-05-06 20:54:43 +0200

    Initial Setup

---
+++ VendingMachineSpectre/Machine.cs
@@ -0,0 +1,20 @@
+﻿using Spectre.Console;
+
+public class Machine
+{
+    public static void TableBuild(Table table)
+    {
+        table.AddColumn("Products");
+        table.AddRow("Test");
+    }
+    public static void Main(string[] args)
+    {
+        var MachineTable = new Table();
+
+        TableBuild(MachineTable);
+        AnsiConsole.Write(MachineTable);
+
+
+        Console.ReadKey();
+    }
+}
