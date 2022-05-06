2699d0687496bf275352335fc877fb9969d5466b
HUNTERSCOUTI <39131351+HUNTERSCOUTI@users.noreply.github.com>
2022-05-06 21:18:40 +0200

    Fullscreenupdate

--- VendingMachineSpectre/Machine.cs
+++ VendingMachineSpectre/Machine.cs
@@ -1,7 +1,20 @@
 ﻿using Spectre.Console;
+using System.Runtime.InteropServices;
 
 public class Machine
 {
+    #region Fullscreen Variables
+    [DllImport("kernel32.dll", ExactSpelling = true)]
+    private static extern IntPtr GetConsoleWindow();
+    private static IntPtr ThisConsole = GetConsoleWindow();
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    private const int HIDE = 0;
+    private const int MAXIMIZE = 3;
+    private const int MINIMIZE = 6;
+    private const int RESTORE = 9;
+    #endregion
+
     public static void TableBuild(Table table)
     {
         table.AddColumn("Products");
@@ -9,6 +22,10 @@ public class Machine
     }
     public static void Main(string[] args)
     {
+        Console.Title = "Match 3";
+        Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+        ShowWindow(ThisConsole, MAXIMIZE);
+
         var MachineTable = new Table();
 
         TableBuild(MachineTable);
